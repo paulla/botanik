7bc0496134ce1289336b16a726ecf2f41c03466b
EthPyth <EthPyth@users.noreply.github.com>
2015-01-30 13:41:01 +0100

    Update Politesse.py

--- src/paulla.ircbot/src/paulla/ircbot/plugins/Politesse.py
+++ src/paulla.ircbot/src/paulla/ircbot/plugins/Politesse.py
@@ -41,7 +41,7 @@ class Politesse:
                     self.bot.call_with_human_delay(
                             self.bot.privmsg,
                             channel,
-                            "%s, %s" % (mask.nick, self.reply[random.randint(0, len(reply)-1)]))
+                            "%s, %s" % (mask.nick, self._reply[random.randint(0, len(self._reply)-1)]))
             elif mask.nick.lower() == self.bot.nick.lower():
                 self.usersreplied = [nick.lower() for nick in self.bot.nicks]
     
