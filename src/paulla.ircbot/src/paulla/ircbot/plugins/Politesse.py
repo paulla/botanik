db6dee6968bec3e8f27a416d82bd7fa9cd35d518
EthPyth <EthPyth@users.noreply.github.com>
2015-02-02 17:42:48 +0100

    Update Politesse.py

--- src/paulla.ircbot/src/paulla/ircbot/plugins/Politesse.py
+++ src/paulla.ircbot/src/paulla/ircbot/plugins/Politesse.py
@@ -35,16 +35,12 @@ class Politesse:
     @irc3.event(irc3.rfc.PRIVMSG)
     def bienlebonjour(self, mask, event, target, data):  
         if [word for word in self._hi if '%s' % word in data.lower()]:
-            if mask.nick != self.bot.nick:
-                #and not mask.nick.lower() in self.usersreplied:
-                #   self.usersreplied.append(mask.nick.lower())
-                self.bot.privmsg(target, "%s, %s" % (mask.nick, self._reply[random.randint(0, len(self._reply)-1)]))
-                #self.bot.call_with_human_delay(
-                #        self.bot.privmsg,
-                #        channel,
-                #        "%s, %s" % (mask.nick, self._reply[random.randint(0, len(self._reply)-1)]))
-            #elif mask.nick.lower() == self.bot.nick.lower():
-                #self.usersreplied = [nick.lower() for nick in self.bot.nicks]
+            if mask.nick != self.bot.nick/
+                and not mask.nick.lower() in self.usersreplied:
+                    self.usersreplied.append(mask.nick.lower())
+                    self.bot.privmsg(target, "%s, %s" % (mask.nick, self._reply[random.randint(0, len(self._reply)-1)]))
+            elif mask.nick.lower() == self.bot.nick.lower():
+                self.usersreplied = [nick.lower() for nick in self.bot.nicks]
     
 
     @cron('0 8 * * *')
