a75fdc2d9dac8ea724fb3152b90aadd81d3c4134
EthPyth <EthPyth@users.noreply.github.com>
2015-02-07 16:57:43 +0100

    Update services.py

--- src/paulla.ircbot/src/paulla/ircbot/plugins/services.py
+++ src/paulla.ircbot/src/paulla/ircbot/plugins/services.py
@@ -15,11 +15,12 @@ class ServicesPaulla:
     
     @irc3.event(irc3.rfc.PRIVMSG)
     def services_paulla(self, mask, event, target, data):
-        if [values for values in thedict.values() for values in values if '%s' % values in data]:
-            if mask.nick != self.bot.nick:
-                valuestokeep = [values for values in thedict.values() for values in values if '%s' % values in data]
-                listkeys = [keys for keys, values in thedict.items() for values in values if values in valuestokeep]
-                if listkeys:
-                    for thekey in listkeys:
-                        self.bot.privmsg(target, "%s, il y a ce service au besoin : https://%s.paulla.asso.fr/" % (mask.nick, thekey))
+        if '!service' in data:
+            if [values for values in thedict.values() for values in values if '%s' % values in data]:
+                if mask.nick != self.bot.nick:
+                    valuestokeep = [values for values in thedict.values() for values in values if '%s' % values in data]
+                    listkeys = [keys for keys, values in thedict.items() for values in values if values in valuestokeep]
+                    if listkeys:
+                        for thekey in listkeys:
+                            self.bot.privmsg(target, "%s, il y a ce service au besoin : https://%s.paulla.asso.fr/" % (mask.nick, thekey))
       
